2047bda854afe3a035bbb49f4939e04b1c3a73d0
pratishthaverma22 <30427779+pratishthaverma22@users.noreply.github.com>
2020-05-22 23:03:40 +0530

    Update CountSquareSubmatricesWithAllOnes.py

--- Week 3/CountSquareSubmatricesWithAllOnes.py
+++ Week 3/CountSquareSubmatricesWithAllOnes.py
@@ -42,10 +42,10 @@ class Solution:
         for r in range(rows):
             for c in range(cols):
                 if matrix[r][c] == 1:
-                    if r == 0 or c == 0: # Cases with first row or first col
+                    if r == 0 or c == 0: # Cases with first row or first column
                         result += 1      # The 1 cells are square on its own               
                     else:                # Other cells
                         cell_val = min(matrix[r-1][c-1], matrix[r][c-1], matrix[r-1][c]) + matrix[r][c]
                         result += cell_val
-                        matrix[r][c] = cell_val #**memoize the updated result**
+                        matrix[r][c] = cell_val 
         return result  
